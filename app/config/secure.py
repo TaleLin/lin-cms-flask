44e7bcbe79beaf8f678e5a1481ae81f29e7ff753
pedro <gaopedro@163.com>
2019-03-04 13:58:32 +0800

    doc: 更新README.md

--- app/config/secure.py
+++ app/config/secure.py
@@ -6,4 +6,6 @@
 # 安全性配置
 SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:123456@localhost:3306/lin-cms'
 
+SQLALCHEMY_ECHO = False
+
 SECRET_KEY = '\x88W\xf09\x91\x07\x98\x89\x87\x96\xa0A\xc68\xf9\xecJJU\x17\xc5V\xbe\x8b\xef\xd7\xd8\xd3\xe6\x95*4'
