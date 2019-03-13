69124c992dd531d299f06e6f8cefc397472349e3
fujiale33 <fujiale3@qq.com>
2019-03-13 21:32:34 +0800

    feat: 插件初始化支持自动检测依赖冲突问题

--- plugin_init.py
+++ plugin_init.py
@@ -2,9 +2,10 @@
     :copyright: © 2019 by the Lin team.
     :license: MIT, see LICENSE for more details.
 """
+import json
 import re
 from importlib import import_module
-
+import subprocess
 import os
 
 from app.app import create_app
@@ -51,6 +52,10 @@ class PluginInit:
             }
 
     def auto_install_rely(self):
+        try:
+            DependenciesResolve(app, self.path_info)
+        except Exception as e:
+            raise Exception('安装插件依赖时发生错误！\nError:' + str(e))
         from subprocess import CalledProcessError
         for name in self.path_info:
             filename = 'requirements.txt'
@@ -93,7 +98,7 @@ class PluginInit:
             try:
                 info_mod = import_module(self.path_info[name]['plugin_info_path'])
             except ModuleNotFoundError as e:
-                raise exit(str(e) + '\n未找到插件' + name + '，请检查您输入的插件名是否正确')
+                raise Exception(str(e) + '\n未找到插件' + name + '，请检查您输入的插件名是否正确')
 
             res = self._generate_setting(name, info_mod)
             setting_text[name] = res
@@ -109,9 +114,8 @@ class PluginInit:
                 plugin_module = import_module(self.path_info[name]['plugin_path'] + '.app.__init__')
                 dir_info = dir(plugin_module)
             except ModuleNotFoundError as e:
-                raise exit(str(e) + '\n未找到插件' + name + '，请检查您输入的插件名是否正确')
+                raise Exception(str(e) + '\n未找到插件' + name + '，请检查您输入的插件名是否正确或插件中是否有未安装的依赖包')
             if 'initial_data' in dir_info:
-                # TODO 解决多次初始化数据重复添加的问题
                 plugin_module.initial_data()
         print('插件初始化成功')
 
@@ -158,7 +162,6 @@ class PluginInit:
 
     @classmethod
     def __execute_cmd(cls, cmd):
-        import subprocess
         code = subprocess.check_call(cmd, shell=True, stdout=subprocess.PIPE)
         if code == 0:
             return True
@@ -206,6 +209,145 @@ class PluginInit:
         return final_setting
 
 
+class DependenciesResolve:
+
+    def __init__(self, app_obj, path):
+        self.app = app_obj
+        self.path_info = path
+        # 主项目的依赖关系列表
+        self.root_graph = []
+        # 所有插件的依赖关系列表
+        self.plugin_graph = []
+        # 生成主项目和插件依赖关系列表
+        self.generate_graph()
+        self.check_dependencies()
+
+    def generate_graph(self):
+        try:
+            p = subprocess.Popen(["pipenv", "graph", "--json"],
+                                 stdout=subprocess.PIPE)
+
+            r = p.communicate()[0].decode('utf-8')
+            self.root_graph = json.loads(r)
+
+        except subprocess.CalledProcessError as e:
+            exit("pipenv指令未安装，请先使用pip安装命令\nError" + str(e))
+
+        except Exception as e:
+            exit("pipenv 错误，请检测你的pipenv配置！\nError：" + str(e))
+
+        self.__generate_plugin_graph()
+
+    def check_dependencies(self):
+        for package in self.root_graph:
+            # 验证顶级包是否符合规范
+            self.__check_top_dependencies(package['package'])
+
+            # 验证子包是否符合规范
+            self.__check_sub_dependencies(package['dependencies'])
+
+    def __generate_plugin_graph(self):
+        for name, val in self.path_info.items():
+            # 首先去校验插件依赖于住项目的依赖是否存在冲突
+            plugin_path = self.path_info[name]['plugin_path'].replace(
+                '.', '/').replace('app', '')
+            requirements_path = self.app.config.root_path + \
+                plugin_path + '/requirements.txt'
+            with open(requirements_path, 'r') as f:
+                while True:
+
+                    # 正则匹配requirements的每一行的信息
+                    line = f.readline()
+                    if not line:
+                        break
+
+                    pattern = '(.*?)(==|<=|>=|!=|>|<)(.*)'
+                    search_obj = re.search(pattern, line)
+                    if search_obj:
+
+                        package_name = search_obj.group(1)
+                        condition = search_obj.group(2)
+                        version = search_obj.group(3)
+                        key = search_obj.group(1).lower()
+
+                        plugin_package = dict({'package': {}})
+                        plugin_package['package']['key'] = key
+                        plugin_package['package']['package_name'] = package_name
+                        plugin_package['package']['version'] = version
+                        plugin_package['package']['condition'] = condition
+                        plugin_package['package']['plugin_name'] = name
+                        self.plugin_graph.append(plugin_package)
+
+    def __check_top_dependencies(self, top_package):
+            for plugin_package in self.plugin_graph:
+                # top_version = top_package['installed_version']
+                # plugin_version = plugin_package['version']
+                if top_package['key'] == plugin_package['package']['key']:
+                    err_msg = '由于项目主目录已经存在在包' \
+                              '' + top_package['package_name'] + '，但 ' + plugin_package['package']['plugin_name']\
+                              + ' 插件尝试重复安装不同版本，请尝试手动去掉该插件的requirements.txt中的包'
+                    raise Exception(err_msg)
+
+    def __check_sub_dependencies(self, dep_package):
+        # 判断插件中要安装的依赖，是否符合主项目已安装的依赖规定的范围
+        for dependence in dep_package:
+            required_version = dependence['required_version']
+            dep_name = dependence['key']
+
+            if required_version is not None:
+                version_infos = required_version.split(",")
+                for version_info in version_infos:
+                    pattern = '(>=|<=|!=|==|<|>)(.*)'
+                    search_obj = re.search(pattern, version_info)
+                    condition = search_obj.group(1)
+                    version = int(search_obj.group(2).replace('.', ''))
+
+                    for plugin_package in self.plugin_graph:
+                        name = plugin_package['package']['key']
+                        if dep_name == name:
+                            plugin_package_version = int(plugin_package['package']['version'].replace('.', ''))
+                            err_msg = plugin_package['package']['plugin_name'] + '插件的依赖 ' + name +\
+                                ' 与主项目依赖版本发生冲突! 请自行手动解决'
+                            if condition == '>=':
+                                if plugin_package_version >= version:
+                                    pass
+                                else:
+                                    raise Exception(err_msg)
+
+                            elif condition == '==':
+                                if plugin_package_version == version:
+                                    pass
+                                else:
+                                    raise Exception(err_msg)
+
+                            elif condition == '!=':
+                                if plugin_package_version != version:
+                                    pass
+                                else:
+                                    raise Exception(err_msg)
+
+                            elif condition == '<=':
+                                if plugin_package_version <= version:
+                                    pass
+                                else:
+                                    raise Exception(err_msg)
+
+                            elif condition == '<':
+                                if plugin_package_version < version:
+                                    pass
+                                else:
+                                    raise Exception(err_msg)
+
+                            elif condition == '>':
+                                if plugin_package_version > version:
+                                    pass
+                                else:
+                                    raise Exception(err_msg)
+            else:
+                pass
+
+
 if __name__ == '__main__':
+    app = create_app(register_all=False)
     plugin_name = input('请输入要初始化的插件名，如果多个插件请使用空格分隔插件名，输入*表示初始化所有插件:\n')
     PluginInit(plugin_name)
