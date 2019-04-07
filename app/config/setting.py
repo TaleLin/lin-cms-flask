7a60654c7bdca8d4c3d0b942c7140d47c68fe040
fujiale33 <fujiale3@qq.com>
2019-04-08 00:25:17 +0800

    fix: 增加配置项目，屏蔽sql alchemy的FSADeprecationWarning

--- app/config/setting.py
+++ app/config/setting.py
@@ -12,9 +12,12 @@ PAGE_DEFAULT = 0
 # 令牌配置
 JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 
+# 屏蔽 sql alchemy 的 FSADeprecationWarning
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # 插件模块暂时没有开启，以下配置可忽略
 # plugin config写在字典里面
 PLUGIN_PATH = {
-    'poem': {'path': 'app.plugins.poem', 'enable': True, 'version': '0.0.1', 'limit': 20},
-    'oss': {'path': 'app.plugins.oss', 'enable': True, 'version': '0.0.1', 'access_key_id': 'not complete', 'access_key_secret': 'not complete', 'endpoint': 'http://oss-cn-shenzhen.aliyuncs.com', 'bucket_name': 'not complete', 'upload_folder': 'app', 'allowed_extensions': ['jpg', 'gif', 'png', 'bmp']}
+    'oss': {'path': 'app.plugins.oss', 'enable': True, 'version': '0.0.1', 'access_key_id': 'not complete', 'access_key_secret': 'not complete', 'endpoint': 'http://oss-cn-shenzhen.aliyuncs.com', 'bucket_name': 'not complete', 'upload_folder': 'app', 'allowed_extensions': ['jpg', 'gif', 'png', 'bmp']},
+    'poem': {'path': 'app.plugins.poem', 'enable': True, 'version': '0.0.1', 'limit': 20}
 }
