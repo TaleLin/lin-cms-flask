26c630e1f940f02175a9ffd10c0c7c49043e02b4
fujiale33 <fujiale3@qq.com>
2019-04-08 09:18:37 +0800

    feat: 增加 add_super 脚本的成功提示

--- app/config/setting.py
+++ app/config/setting.py
@@ -18,6 +18,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # 插件模块暂时没有开启，以下配置可忽略
 # plugin config写在字典里面
 PLUGIN_PATH = {
-    'oss': {'path': 'app.plugins.oss', 'enable': True, 'version': '0.0.1', 'access_key_id': 'not complete', 'access_key_secret': 'not complete', 'endpoint': 'http://oss-cn-shenzhen.aliyuncs.com', 'bucket_name': 'not complete', 'upload_folder': 'app', 'allowed_extensions': ['jpg', 'gif', 'png', 'bmp']},
-    'poem': {'path': 'app.plugins.poem', 'enable': True, 'version': '0.0.1', 'limit': 20}
+    'poem': {'path': 'app.plugins.poem', 'enable': True, 'version': '0.0.1', 'limit': 20},
+    'oss': {'path': 'app.plugins.oss', 'enable': True, 'version': '0.0.1', 'access_key_id': 'not complete', 'access_key_secret': 'not complete', 'endpoint': 'http://oss-cn-shenzhen.aliyuncs.com', 'bucket_name': 'not complete', 'upload_folder': 'app', 'allowed_extensions': ['jpg', 'gif', 'png', 'bmp']}
 }
