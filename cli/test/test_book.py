e0f6740405c8003b4df698af40514b41f7dbdc51
sunlin92 <sun.melodies@gmail.com>
2020-11-25 17:42:48 +0800

    调整cli脚本

--- cli/test/test_book.py
+++ cli/test/test_book.py
@@ -2,10 +2,18 @@
     :copyright: © 2020 by the Lin team.
     :license: MIT, see LICENSE for more details.
 """
-from .util import get_token
 if __name__ == "__main__":
     import sys
+    import os
+    current_path = os.path.abspath(__file__)
+    # 获取当前文件的父目录
+    father_path = os.path.abspath(
+        os.path.dirname(current_path) + os.path.sep + ".")
+
+    os.chdir(father_path)
+    # 切换到项目根目录
     sys.path.append("../../")
+from cli.test.util import get_token
 from app.app import create_app
 
 app = create_app()
--- cli/test/util.py
+++ cli/test/util.py
@@ -1,13 +1,22 @@
 import json
 
 
+def get_file_path():
+    import os
+    current_path = os.path.abspath(__file__)
+    # 获取当前文件的父目录
+    father_path = os.path.abspath(
+        os.path.dirname(current_path) + os.path.sep + ".")
+    return father_path+os.path.sep+"token.json"
+
+
 def write_token(data):
     obj = json.dumps(data)
-    with open('token.json', 'w') as f:
+    with open(get_file_path(), 'w') as f:
         f.write(obj)
 
 
 def get_token(key='access_token'):
-    with open('token.json', 'r') as f:
+    with open(get_file_path(), 'r') as f:
         obj = json.loads(f.read())
         return obj[key]
