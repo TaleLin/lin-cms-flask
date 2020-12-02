3d5e84c6889cdc1d729a65f6944889d30733d1e0
sunlin92 <sun.melodies@gmail.com>
2020-12-02 22:38:33 +0800

    fix:gunicore 多进程启动加载不到指定的环境变量

--- app/app.py
+++ app/app.py
@@ -3,11 +3,11 @@
     :license: MIT, see LICENSE for more details.
 """
 
+import os
 
 from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
-
 from lin import Lin
 
 
@@ -23,12 +23,10 @@ def apply_cors(app):
     CORS(app)
 
 
-def load_config(app):
+def load_app_config(app):
     """
     根据指定配置环境自动加载对应环境变量和配置类
     """
-    # 兼容 其他HTTP Server托管启动, 手动读取环境配置
-    load_dotenv(".flaskenv")
     # 根据传入环境加载对应配置
     env = app.config.get("ENV")
     # 读取 .env
@@ -42,8 +40,10 @@ def load_config(app):
 
 
 def create_app(register_all=True, **kwargs):
+    # http wsgi server托管启动需指定读取环境配置
+    load_dotenv(".flaskenv")
     app = Flask(__name__, static_folder="./assets")
-    load_config(app)
+    load_app_config(app)
     if register_all:
         register_blueprints(app)
         Lin(app, **kwargs)
