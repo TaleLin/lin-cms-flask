06bf15f6485653ab1f7fd34e08a2fe501704f33d
sunlin92 <sun.melodies@gmail.com>
2020-12-11 16:37:43 +0800

    feat: g params

--- app/validator/schema.py
+++ app/validator/schema.py
@@ -1,8 +1,10 @@
 from enum import Enum
-from typing import List, Optional
+from inspect import Parameter
+from typing import Any, List, Optional
 
 from flask import g
 from lin import BaseModel
+from lin.exception import ParameterError
 from pydantic import Field
 
 datetime_regex = "^((([1-9][0-9][0-9][0-9]-(0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|(20[0-3][0-9]-(0[2469]|11)-(0[1-9]|[12][0-9]|30))) (20|21|22|23|[0-1][0-9]):[0-5][0-9]:[0-5][0-9])$"
@@ -21,15 +23,39 @@ class QuerySearchSchema(BaseModel):
     count: int = Field(5, gt=0, lt=16, description="0 < count < 16")
     page: int = 0
 
-    @staticmethod
-    def before_handler(req, resp, req_validation_error, instance):
-        g.offset = req.context.query.count * req.context.query.page
+def g_params_handler(req, resp, req_validation_error, instance):
+    schemas = ["cookies","headers","query","json"]
+    for schema in schemas:
+        params = getattr(req.context, schema)
+        if params:
+            for k, v in params:
+                if hasattr(g, k):
+                    raise ParameterError("parameter name {k} in {schema} is Duplicated ".format(schema=schema,k=k))
+                setattr(g, k, v)
+    g.offset = req.context.query.count * req.context.query.page
 
-    @staticmethod
-    def after_handler(req, resp, req_validation_error, instance):
-        pass
 
+class LogSchema(BaseModel):
+    id:int
+    message :str
+    user_id :int
+    username :str
+    status_code :int
+    method :str
+    path :str
+    permission :str
 
+class BaseListSchema(BaseModel):
+    page:int
+    count:int
+    total:int
+    total_page:int
+    items:List[Any]
+
+class LogListSchema(BaseListSchema):
+    items:List[LogSchema]
+    
+    
 class AccessTokenSchema(BaseModel):
     Authorization: str
 
@@ -51,7 +77,7 @@ class BookSchema(BaseModel):
 
 
 class BookListSchema(BaseModel):
-    books: List[BookSchema] = []
+    items: List[BookSchema] = []
 
 
 class Language(str, Enum):
