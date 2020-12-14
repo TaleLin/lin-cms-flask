4ab428e8259764990021256c19277d9ee481547e
sunlin92 <sun.melodies@gmail.com>
2020-12-14 17:18:51 +0800

    fix: gunicorn config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,8 +6,8 @@ import multiprocessing
 
 bind = "0.0.0.0:5000"
 pidfile = "/tmp/gunicorn.pid"
-accesslog = "./logs/gunicorn.log"
+accesslog = "/var/log/gunicorn.log"
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "gevent"
+worker_class = 'gevent'
 daemon = True
 debug = False
