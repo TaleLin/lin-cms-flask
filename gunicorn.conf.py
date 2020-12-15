58e25776b3e2768d1a5318b083fd692d95fe7d0f
sunlin92 <sun.melodies@gmail.com>
2020-12-15 13:56:49 +0800

    fix:不指定文件位置避免权限问题

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,9 +5,9 @@ monkey.patch_all()
 import multiprocessing
 
 bind = "0.0.0.0:5000"
-pidfile = "/tmp/gunicorn.pid"
-accesslog = "/var/log/gunicorn.log"
-workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'gevent'
 daemon = False
+workers = multiprocessing.cpu_count() * 2 + 1
 debug = False
+# pidfile = "/var/run/gunicorn.pid"
+# accesslog = "/var/log/gunicorn.log"
