fd5529d26bd283944f216f2cdd9712258ec35248
sunlin92 <sun.melodies@gmail.com>
2020-12-21 14:57:54 +0800

    refactor:permissionMeta参数重命名 close #128

--- app/api/cms/user.py
+++ app/api/cms/user.py
@@ -33,7 +33,7 @@ user_api = Redprint("user")
 
 
 @user_api.route("/register", methods=["POST"])
-@permission_meta(auth="注册", module="用户", mount=False)
+@permission_meta(name="注册", module="用户", mount=False)
 @Logger(template="管理员新建了一个用户")  # 记录日志
 @admin_required
 def register():
@@ -48,7 +48,7 @@ def register():
 
 
 @user_api.route("/login", methods=["POST"])
-@permission_meta(auth="登录", module="用户", mount=False)
+@permission_meta(name="登录", module="用户", mount=False)
 def login():
     form = LoginForm().validate_for_api()
     user = manager.user_model.verify(form.username.data, form.password.data)
@@ -68,7 +68,7 @@ def login():
 
 
 @user_api.route("", methods=["PUT"])
-@permission_meta(auth="用户更新信息", module="用户", mount=False)
+@permission_meta(name="用户更新信息", module="用户", mount=False)
 @login_required
 def update():
     form = UpdateInfoForm().validate_for_api()
@@ -92,7 +92,7 @@ def update():
 
 
 @user_api.route("/change_password", methods=["PUT"])
-@permission_meta(auth="修改密码", module="用户", mount=False)
+@permission_meta(name="修改密码", module="用户", mount=False)
 @Logger(template="{user.username}修改了自己的密码")  # 记录日志
 @login_required
 def change_password():
@@ -107,7 +107,7 @@ def change_password():
 
 
 @user_api.route("/information", methods=["GET"])
-@permission_meta(auth="查询自己信息", module="用户", mount=False)
+@permission_meta(name="查询自己信息", module="用户", mount=False)
 @login_required
 def get_information():
     current_user = get_current_user()
@@ -115,7 +115,7 @@ def get_information():
 
 
 @user_api.route("/refresh", methods=["GET"])
-@permission_meta(auth="刷新令牌", module="用户", mount=False)
+@permission_meta(name="刷新令牌", module="用户", mount=False)
 def refresh():
     try:
         verify_jwt_refresh_token_in_request()
@@ -132,7 +132,7 @@ def refresh():
 
 
 @user_api.route("/permissions", methods=["GET"])
-@permission_meta(auth="查询自己拥有的权限", module="用户", mount=False)
+@permission_meta(name="查询自己拥有的权限", module="用户", mount=False)
 @login_required
 def get_allowed_apis():
     user = get_current_user()
