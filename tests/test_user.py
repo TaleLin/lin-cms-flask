fe0cf8cdaa58cc16707ab14150887546a1c9e774
sunlin92 <sun.melodies@gmail.com>
2020-12-26 16:13:56 +0800

    test:优化测试用例

---
+++ tests/test_user.py
@@ -0,0 +1,17 @@
+"""
+    :copyright: © 2020 by the Lin team.
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+from . import app, fixtureFunc, get_token
+
+
+def test_change_nickname(fixtureFunc):
+    with app.test_client() as c:
+        rv = c.put(
+            "/cms/user",
+            headers={"Authorization": "Bearer " + get_token()},
+            json={"nickname": "tester"},
+        )
+        assert rv.status_code == 201
